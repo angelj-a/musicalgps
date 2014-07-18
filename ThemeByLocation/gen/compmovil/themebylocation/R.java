0e75f3fb90285f2797e27bf38bdee6c72c7a7b69
Angel Abregú <aj.abregu@gmail.com>
2014-07-18 01:50:13 -0300

    Regions can be edited (in progress)

--- ThemeByLocation/gen/compmovil/themebylocation/R.java
+++ ThemeByLocation/gen/compmovil/themebylocation/R.java
@@ -12,25 +12,34 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ic_menu_edit=0x7f020001;
-        public static final int ic_menu_mapmode=0x7f020002;
+        public static final int ic_menu_add=0x7f020001;
+        public static final int ic_menu_edit=0x7f020002;
+        public static final int ic_menu_mapmode=0x7f020003;
+        public static final int ic_menu_refresh=0x7f020004;
     }
     public static final class id {
+        public static final int add_region=0x7f08000a;
         public static final int info_text=0x7f080003;
-        public static final int manage_regions=0x7f080006;
-        public static final int spinner1=0x7f080005;
+        public static final int manage_regions=0x7f080009;
+        public static final int refresh_table=0x7f08000b;
+        public static final int region_latitude_edit=0x7f080005;
+        public static final int region_longitude_edit=0x7f080006;
+        public static final int region_name_edit=0x7f080004;
+        public static final int spinner1=0x7f080008;
         public static final int startbutton=0x7f080002;
         public static final int stopservicebutton=0x7f080001;
-        public static final int tableRegions=0x7f080004;
+        public static final int tableRegions=0x7f080007;
         public static final int unbindbutton=0x7f080000;
     }
     public static final class layout {
         public static final int main_layout=0x7f030000;
-        public static final int regionsadmin_layout=0x7f030001;
-        public static final int spinner_themes=0x7f030002;
+        public static final int region_edit_dialog=0x7f030001;
+        public static final int regionsadmin_layout=0x7f030002;
+        public static final int spinner_themes=0x7f030003;
     }
     public static final class menu {
         public static final int options_menu=0x7f070000;
+        public static final int region_editor_menu=0x7f070001;
     }
     public static final class raw {
         public static final int region105=0x7f040000;
@@ -38,8 +47,10 @@ public final class R {
         public static final int region120=0x7f040002;
     }
     public static final class string {
+        public static final int add_region=0x7f050008;
         public static final int app_name=0x7f050000;
         public static final int manage_regions=0x7f050006;
+        public static final int refresh_table=0x7f050009;
         public static final int start_service=0x7f050005;
         public static final int str_manual_stop_song=0x7f050004;
         public static final int str_stop_service=0x7f050003;
--- ThemeByLocation/src/compmovil/themebylocation/dbeditor/EditorActivity.java
+++ ThemeByLocation/src/compmovil/themebylocation/dbeditor/EditorActivity.java
@@ -1,44 +1,40 @@
 package compmovil.themebylocation.dbeditor;
 
+import java.util.List;
+
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import compmovil.themebylocation.R;
-import compmovil.themebylocation.models.ThemesManager;
 
 public class EditorActivity extends Activity {
 	
 	private RegionEditor mRegionEditor;
 	private ThemeSelector mThemeSelector;
-	//private RegionsDatabase mRegionsDatabase;
+
 	
+	private DBAdapter mRegionsThemesDB;
 	
 //	private ThemesManager mThemesManager;
 //	private Spinner mDropdown;
 //	private ArrayAdapter<String> mAdapter;
 	
+	TableLayout mTable;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regionsadmin_layout);
-        
-        RegionsDbHelper dbHelper = new RegionsDbHelper(getBaseContext());
-        
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-      
-        
-        mRegionEditor = new RegionEditor(this);
-        mThemeSelector = new ThemeSelector(this);
-        
+                
 		initializeRegionsAdmin();
-		initializeTable();
+		showTable();
         
 //		mThemesManager = new ThemesManager();
 //		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mThemesManager.getThemesNames());
@@ -80,6 +76,7 @@ public class EditorActivity extends Activity {
 
 	@Override
     protected void onDestroy(){
+		mRegionsThemesDB.close();
 		super.onDestroy();
 	}
 	
@@ -90,12 +87,39 @@ public class EditorActivity extends Activity {
 	}
 	
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.region_editor_menu, menu);
+	    //mMenu = menu;
+	    return true;
+	}
 	
-	private void initializeTable() {
-		TableLayout table = (TableLayout) findViewById(R.id.tableRegions);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.add_region:	
+	        	mRegionEditor.addRegion();
+	            return true;
+	        case R.id.refresh_table:
+	        	refreshTable();
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}	
+	
+	
 
+	private void showTable() {		
+		List<RegionModel> listOfRegions = mRegionsThemesDB.getAllRegions();
+		List<String> listOfThemesNames = mRegionsThemesDB.getAllThemesNames();
+		
+		if (mTable == null)
+			mTable = (TableLayout) findViewById(R.id.tableRegions);
 
-	    for (int i = 0; i < 6; i++) {
+	    for (int i = 0; i < listOfRegions.size(); i++) {
 
 	        TableRow row= new TableRow(this);
 	        TableRow.LayoutParams layoutParams= new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
@@ -105,6 +129,12 @@ public class EditorActivity extends Activity {
 	        TextView regionName = new TextView(this);
 	        TextView themeName = new TextView(this);
 	        
+	        //To identify its position
+	        regionName.setId(i);
+	        themeName.setId(i);
+	        
+	        
+	        //Appearance
 	        idRow.setTextSize(24);
 	        regionName.setTextSize(24);
 	        themeName.setTextSize(24);
@@ -112,9 +142,10 @@ public class EditorActivity extends Activity {
 	        idRow.setText(Integer.toString(i+1));
 	        idRow.setPaddingRelative(10, 10, 20, 10);
 	        idRow.setTextColor(Color.parseColor("#FFFFFF"));
-	        regionName.setText("REGIÓN " + i);
+	        regionName.setText(listOfRegions.get(i).getName());
 	        regionName.setPaddingRelative(2, 10, 2, 10);
-	        themeName.setText("ALGÚN TEMA");
+	        themeName.setText(listOfThemesNames
+	        					.get(listOfRegions.get(i).getThemeId()));
 	        themeName.setPaddingRelative(2, 10, 20, 10);
 	        
 	        if (i%2 == 0){
@@ -136,15 +167,24 @@ public class EditorActivity extends Activity {
 	        row.addView(regionName);
 	        row.addView(themeName);
 	       
-	        table.addView(row,i);
+	        mTable.addView(row,i);
 	    }
 		
+	}
+	
+	public void refreshTable(){
+ 		mTable.removeAllViews();
+ 		showTable();
 	}
 
 
 	private void initializeRegionsAdmin() {
-		// TODO Auto-generated method stub
-		
+		mRegionsThemesDB = new DBAdapter(this);
+		mRegionsThemesDB.open(false);
+        
+        
+        mRegionEditor = new RegionEditor(this, mRegionsThemesDB);
+        mThemeSelector = new ThemeSelector(this);	
 	}
 	
 }
